65f8eeb9ad19569b8f41e79ae2d1368912dbdeb2
BudgetingHacks <ticketsmetro1@gmail.com>
2025-09-15 19:48:48 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,14 @@
-const CACHE_NAME = "budget-cache-v1";
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/icon.png" // add any extra files like CSS, JS, images here
-];
+const CACHE_NAME = "budget-app-cache-v1";
+const urlsToCache = ["/", "/index.html", "/icon.png"];
 
-// Install
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-// Activate
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Fetch
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
@@ -37,4 +16,3 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
-
