b263ad992f4635e711d1546d754e5afa2d9a597a
BudgetingHacks <ticketsmetro1@gmail.com>
2025-09-15 21:33:28 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,24 @@
-const CACHE_NAME = "budget-app-v1";
-const urlsToCache = ["./", "./index.html"];
+const CACHE_NAME = 'budget-app-v1';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
 
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+self.addEventListener('install', evt => {
+  evt.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(k => { if(k !== CACHE_NAME) return caches.delete(k); })
+    ))
+  );
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(caches.match(evt.request).then(resp => resp || fetch(evt.request)));
 });
