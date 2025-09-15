621152e45b0f19dc8b89df1710e60e75f33e392e
ticketsmetro1-stack <ticketsmetro1@gmail.com>
2025-09-15 18:08:52 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,40 @@
+const CACHE_NAME = "budget-cache-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/icon.png" // add any extra files like CSS, JS, images here
+];
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// Activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
 
